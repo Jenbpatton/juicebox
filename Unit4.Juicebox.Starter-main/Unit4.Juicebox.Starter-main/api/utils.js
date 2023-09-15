48802dd2ec08ac2657cc7fe821131c4ac9a901b5
Jennifer Patton <jenbuckleypatton@gmail.com>
2023-09-15 13:12:36 -0700

    create a middleware function

--- Unit4.Juicebox.Starter-main/Unit4.Juicebox.Starter-main/api/utils.js
+++ Unit4.Juicebox.Starter-main/Unit4.Juicebox.Starter-main/api/utils.js
@@ -1,7 +1,16 @@
 function requireUser(req, res, next) {
+  if (req.user){
+//check if user is logged in and if the user is logged in, attach the user to the request objec    
+    res.locals.user = req.user;
+    next();
+  } else {
+
+    //if the user is not logged in
+    res.status(401).json({message: 'Unaruhroized'});
+  }
 
 }
 
 module.exports = {
   requireUser
-}
+};
